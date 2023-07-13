b16851edf2188d93bae19eb050018d9f8c0ca17d
vgupta91 <vgupta@kognetics.ai>
2023-07-13 11:03:29 +0530

    changes

--- dashboard.py
+++ dashboard.py
@@ -15,7 +15,7 @@ css = '''
     </style>
 '''
 
-st.set_page_config(page_title='SuperStore!!!', page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title='Dashboard - Streamlit and Plotly!!!', page_icon=":bar_chart:", layout="wide")
 
 st.title(" :bar_chart: Sample superstore EDA")
 
